371bd50605f260fa1d3c5350d020b079bbe8cbc0
nimz19 <izzahzafer@gmail.com>
2024-10-31 10:23:40 +0000

    Draws my initials IZ using chrome browser

--- src/test/java/test/IDrawTest.java
+++ src/test/java/test/IDrawTest.java
@@ -7,37 +7,36 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.PageFactory;
-
-import pages.HomePage;
 
 public class IDrawTest {
 	private static RemoteWebDriver driver;
 	Actions action = new Actions(driver);
-	HomePage nav = PageFactory.initElements(driver, HomePage.class);
 
 	@BeforeClass
 	public static void init() {
-		System.setProperty("webdriver.gecko.driver",
-				"src/test/resources/resources/geckodriver-v0.28.0-win64/geckodriver.exe");
-		FirefoxOptions fOptions = new FirefoxOptions();
-		fOptions.setHeadless(true);
-		fOptions.setBinary("C:\\Users\\edrz\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
-		driver = new FirefoxDriver(fOptions);
-		fOptions.addPreference("profile.default_content_setting_values.cookies", 2);
-		fOptions.addPreference("network.cookie.cookieBehavior", 2);
-		fOptions.addPreference("profile.block_third_party_cookies", true);
-		driver.manage().window().setSize(new Dimension(1366, 768));
+		// Set up the path to the ChromeDriver executable
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\User\\IdeaProjects\\SeleniumCats\\src\\test\\resources\\webdrivers\\chromedriver.exe");
 
+		// Set ChromeOptions for any specific Chrome configurations
+		ChromeOptions cOptions = new ChromeOptions();
+		cOptions.setHeadless(false);  // Change to true if headless mode is preferred
+
+		// Initialize the ChromeDriver with ChromeOptions
+		driver = new ChromeDriver(cOptions);
+
+		// Set the window size
+		driver.manage().window().setSize(new Dimension(1366, 768));
 	}
 
 	@Before
 	public void setup() {
-		driver.get(nav.URL);
+		// Navigate directly to the drawing URL
+		driver.get("https://www.youidraw.com/apps/painter/");
 	}
 
 	@AfterClass
@@ -47,17 +46,42 @@ public class IDrawTest {
 
 	@Test
 	public void drawInital() throws InterruptedException {
+		// Find the drawing canvas
 		WebElement canvas = driver.findElement(By.cssSelector("#catch"));
-		nav.clickBrush();
-		// E
-		action.moveToElement(canvas).clickAndHold().moveByOffset(0, -200).moveByOffset(100, 0).click()
-				.moveToElement(canvas, 0, 0).clickAndHold().moveByOffset(100, 0).click().moveToElement(canvas, 0, 0)
-				.clickAndHold().moveByOffset(0, -100).moveByOffset(100, 0).click().moveToElement(canvas, 0, 0).build()
-				.perform();
-		// R
-		action.moveByOffset(200, 0).clickAndHold().moveByOffset(0, -200).moveByOffset(100, 0).moveByOffset(0, 100)
-				.moveByOffset(-100, 0).moveByOffset(100, 100).build().perform();
-		Thread.sleep(10000);
-	}
 
+		// Start drawing by clicking on the canvas
+		canvas.click();
+
+		// Drawing the letter "I"
+		action.moveToElement(canvas)
+				.clickAndHold()                       // Start at the top of "I"
+				.moveByOffset(0, 200)                 // Draw vertical line down
+				.release()                            // Finish vertical line
+				.perform();
+
+		action.moveByOffset(-50, -200)               // Move to the top-left for the top horizontal line
+				.clickAndHold()
+				.moveByOffset(100, 0)                 // Draw top horizontal line
+				.release()
+				.perform();
+
+		action.moveByOffset(-100, 200)                // Move to the bottom-left for the bottom horizontal line
+				.clickAndHold()
+				.moveByOffset(100, 0)                 // Draw bottom horizontal line
+				.release()
+				.perform();
+
+		Thread.sleep(2000);  // Short pause before drawing the next letter
+
+		// Drawing the letter "Z"
+		action.moveByOffset(200, -200)               // Move to starting position for "Z"
+				.clickAndHold()
+				.moveByOffset(100, 0)                 // Draw top horizontal line
+				.moveByOffset(-100, 200)              // Draw diagonal line down
+				.moveByOffset(100, 0)                 // Draw bottom horizontal line
+				.release()
+				.perform();
+
+		Thread.sleep(10000);  // Wait to observe the drawing, if needed
+	}
 }
